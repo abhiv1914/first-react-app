70d4e48ea4ffe76b512293676e38114b72d8fd27
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-15 10:06:03 +0530

    loops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <>
+      <h1>Props Demo</h1>
+      <ListDemo/>
+    </>
   );
 }
+
+function ListDemo(){
+  let data="Hello World";
+  let list=[];
+  
+  for(let i=0;i<10;i++){
+    list.push("hello Universe");
+  }  
+  console.log(list);
+  return <>
+    <h1>{data} </h1>  
+
+    {/* forEach is consumer function */}
+    {list.forEach((item)=>item)}
+
+    {/* map is input/output */}
+    {/* output because of this */}
+    {list.map((item)=>item)}  
+  </>;
+}
+
 
 export default App;
