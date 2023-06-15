da3ba271191a1bb09c9752b0185760957d72a62e
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-15 18:29:26 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,59 +1,28 @@
 import { useRef, useState } from "react";
 
-function App() {
-  return (
-    <>
-      <h1>Stateful List</h1>
-      <ListDemo />
-    </>
+function App(){
+  return( 
+  <>
+    <h1>My Todo</h1>
+    <MyTodo/>
+  </>
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
+function MyTodo(){
+  let[todo, setTodo] =useState({task: ""});
 
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
-
-    inputRef.current.value = "";
-  };
-
-  return (
+  return(
     <>
-      <input
-        type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
+      <input 
+      className="form-control" 
+      type="text"  
+      placeholder="Enter task"
+      value={todo.task}
       />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
 
-      {list.map((item) => (
-        <MessageDemo message={item} />
-      ))}
+      <input type="button" value="Add Todo" />
     </>
   );
 }
-
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
-    </>
-  );
-}
-
 export default App;
