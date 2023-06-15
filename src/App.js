7040f3c42beb1e09adafcf663dfb16911ac279ca
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-15 09:41:16 +0530

    b4-props

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ function App() {
   return (
     <>
       <h1>Props Demo</h1>
-      
-      <MessageDemo/>
-      <MessageDemo/>
-      <MessageDemo/>
+      {/* Calling the MessageDemo */}
+      <MessageDemo name="abc" email="abc@gmail.com"/>
+      <MessageDemo name="pqr" email="pqr@gmail.com"/>
+      <MessageDemo name="xyz" email="xyz@gmail.com"/>
     </>
   );
 }
-function MessageDemo(){
+
+//Reusable
+// how to make this dynamic
+function MessageDemo({email,name}){
   return(
     <>
-     <h1>Hello World</h1>;
+     <h1>Hello {name} {email}</h1>;
     </>
   );
 }
