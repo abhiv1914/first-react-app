fd411b3d4422f7083b942194b44806cabce547b6
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-15 11:11:52 +0530

    loop-dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import { useState } from "react";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
       <h1>Stateful List</h1>
-      <ListDemo/>
+      <ListDemo />
     </>
   );
 }
 
-function ListDemo(){
-  let [list,setList]=useState["delhi"];
-  let addItemAction =()=>{
-    let newList=[...list,"Mumbai"];
+function ListDemo() {
+  // let list = ["delhi"]
+  // C1: Stateful Variable
+  let [list, setList] = useState(["delhi"]);
+
+  // C2: Acton Member Funcn
+  let addItemAction = () => {
+    // why didn u use push method, push method update the same object
+    // setter method requires immutable object.
+    // We have to pass new list.
+    let newList = [...list, "mumbai"];
     console.log(newList);
 
-    //DOM :: trigger Donm
+    // DOM :: TRIGGER DOM
     setList(newList);
   };
 
-  
-  return ( 
-  <>
-    <input type="button" value="Add New Item" onClick={addItemAction}/>
-    {list.map((item)=>(
-      <h1>Hello {item}</h1>
-    ))}  
-  </>
+  return (
+    <>
+      {/** C3: Event Binding */}
+      <input type="button" value="Add New Item" onClick={addItemAction} />
+
+      {/** C4: List */}
+      {list.map((item) => (
+        <h1>Hello {item}</h1>
+      ))}
+    </>
   );
 }
-
 
 export default App;
